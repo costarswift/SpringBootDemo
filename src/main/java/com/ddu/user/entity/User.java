bf870b50afc07f49fb47b06039dba98116f67c3c
Costar <costarswift@gmail.com>
2026-07-09 17:05:45 +0800

    add createTime and updateDime to User

--- src/main/java/com/ddu/user/entity/User.java
+++ src/main/java/com/ddu/user/entity/User.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Data
 public class User implements Serializable {
@@ -16,4 +17,6 @@ public class User implements Serializable {
     private Integer age;
     private String email;
     private String deleteFlag;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
 }
